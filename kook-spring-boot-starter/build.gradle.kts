e4dd61ebf30f74f9d368124705358a65b9a55979
Enaium <wyy-666@hotmail.com>
2023-04-11 20:22:50 +0800

    subprojects

--- kook-spring-boot-starter/build.gradle.kts
+++ kook-spring-boot-starter/build.gradle.kts
@@ -1,26 +1,18 @@
-plugins {
-    java
-    id("org.springframework.boot") version "3.0.5"
-    id("io.spring.dependency-management") version "1.1.0"
+allprojects {
+    group = "cn.enaium"
+    version = "0.3.0"
+
+    repositories {
+        mavenCentral()
+    }
 }
 
-group = "cn.enaium"
-version = "0.3.0"
-java.sourceCompatibility = JavaVersion.VERSION_17
+subprojects {
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
 
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.boot:spring-boot-starter-websocket")
-
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
+    tasks.withType<Javadoc> {
+        options.encoding = "UTF-8"
+    }
 }
--- kook-spring-boot-starter/settings.gradle.kts
+++ kook-spring-boot-starter/settings.gradle.kts
@@ -1,1 +1,2 @@
-rootProject.name = "kookstart"
+rootProject.name = "root"
+include("kookstarter-webflux")
